14170b33d28e73e6f0869d31e1f9a468f6378895
Hernan Dario Metaute Sarmiento <hernan.metaute@gmail.com>
2012-04-22 20:50:45 -0500

    after posting to the real googl url

--- app/controllers/push_message_controller.rb
+++ app/controllers/push_message_controller.rb
@@ -1,8 +1,13 @@
+require 'net/http'
+require 'uri'
+
 class PushMessageController < ApplicationController
   def send_message
     @message = params[:message]
     @reg_id = Key.find_by_name("registration_id").value
-    
+    postData = Net::HTTP.post_form(URI.parse('https://android.apis.google.com/c2dm/send'), 
+                                   {'registration_id'=>@reg_id, "collapse_key" => "0", "data.payload" => @message})    
+    @message_response = postData.body
   end
 
   
