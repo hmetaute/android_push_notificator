a4936044387067487bb8ac3cd268602f3ac55a07
Hernan Dario Metaute Sarmiento <hernan.metaute@gmail.com>
2012-04-22 21:05:23 -0500

    after changing post logic

--- app/controllers/push_message_controller.rb
+++ app/controllers/push_message_controller.rb
@@ -5,8 +5,9 @@ class PushMessageController < ApplicationController
   def send_message
     @message = params[:message]
     @reg_id = Key.find_by_name("registration_id").value
-    postData = Net::HTTP.post_form(URI.parse('https://android.apis.google.com/c2dm/send'), 
-                                   {'registration_id'=>@reg_id, "collapse_key" => "0", "data.payload" => @message})    
+    uri = URI.parse('https://android.apis.google.com/c2dm/send')
+    params = {'registration_id'=>@reg_id, "collapse_key" => "0", "data.payload" => @message}
+    postData = Net::HTTP.post_form(uri,params)    
     @message_response = postData.body
   end
 
