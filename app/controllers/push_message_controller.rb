9ae68e8f70c011173cbc982caeac46996e22f55f
Hernan Dario Metaute Sarmiento <hernan.metaute@gmail.com>
2012-04-22 22:22:01 -0500

    all logic added for pushnotifications to work on android

--- app/controllers/push_message_controller.rb
+++ app/controllers/push_message_controller.rb
@@ -3,12 +3,24 @@ require 'uri'
 
 class PushMessageController < ApplicationController
   def send_message
+    @auth_key = "DQAAAL8AAACXGEdrjt0wYmkfHZhSrNCMwk503jpx28g93zOUdEN-P1WmfE9Eod1lCbYEUURUuaLJfElRDcsBeYe6XpxxhBTxi39c7vTaz_uikDlJxiYE59wQ9cCGeZ9uz2ROAQFdHuYl-uraJxC4vrYhVaH-m0asYE037u0-_Hz05aBDhagccqEZrvSwUGyqDAxKBxnmXyonEfEcapYls5IgTCb48MMopSzWG5ygVVIBDGjzB2WceK62kVTANcAwgDxaUteBdFc"
+
+    headers = { "Content-Type" => "application/x-www-form-urlencoded", 
+      "Authorization" => "GoogleLogin auth=#{@auth_key}" }
+    
     @message = params[:message]
     @reg_id = Key.find_by_name("registration_id").value
     uri = URI.parse('https://android.apis.google.com/c2dm/send')
-    params = {'registration_id'=>@reg_id, "collapse_key" => "0", "data.payload" => @message}
-    postData = Net::HTTP.post_form(uri,params)    
-    @message_response = postData.body
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    collapse_key = "0"
+    data = "registration_id=#{@reg_id}&collapse_key=#{collapse_key}&data.payload=#{@message}"
+    #req = NET::HTTP::Post.new(uri.path)
+    #params = {'registration_id'=>@reg_id, "collapse_key" => "0", "data.payload" => @message}
+    #postData = Net::HTTP.post_form(uri,params)    
+    resp, dat = http.post(uri.path, data, headers)
+    @message_response = dat + ";" + resp.code.to_s
   end
 
   
