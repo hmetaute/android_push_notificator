4965b4a646aef04c4ac4307e12db2264075c17fe
Hernan Dario Metaute Sarmiento <hernan.metaute@gmail.com>
2012-04-22 20:39:36 -0500

    changed retrieval of data

--- app/controllers/push_message_controller.rb
+++ app/controllers/push_message_controller.rb
@@ -1,7 +1,8 @@
 class PushMessageController < ApplicationController
   def send_message
     @message = params[:message]
-    @reg_id = Key.find_by_name("registration_id")
+    @reg_id = Key.find_by_name("registration_id").value
+    
   end
 
   
