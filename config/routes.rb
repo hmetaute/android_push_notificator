cb03056ade0f04312f89b412174812f85994d9be
Hernan Dario Metaute Sarmiento <hernan.metaute@gmail.com>
2012-04-24 23:46:54 -0500

    changed application root

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ PushNotificator::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'PushMessage#send_message'
 
   # See how all your routes lay out with "rake routes"
 
